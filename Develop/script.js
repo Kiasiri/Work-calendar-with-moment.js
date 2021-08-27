1dabb21d881cc8e9d70d6b4f7be566a2167aab7c
Matthew Wiessing <matthewwiessing@gmail.com>
2021-08-27 16:43:35 -0500

    no real progress

--- Develop/script.js
+++ Develop/script.js
@@ -1,6 +1,13 @@
 var timeBlock = $(".container");
+var saveBtn = $(".saveBtn");
+var eventInfo = $(".event-info");
 function liveClock() {
   const current = moment();
   $("#currentDay").text(current.format("dddd, MMM Do, YYYY, h:mm:ss a"));
 }
 setInterval(liveClock, 1000);
+
+saveBtn.on("click", function () {
+  eventInfo.input;
+  localStorage.setItem("event-info", input);
+});
