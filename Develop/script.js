525213071bce7419d7475909bfe0ab656cf818ba
Matthew Wiessing <matthewwiessing@gmail.com>
2021-08-27 21:56:51 -0500

    added textBox local storage

--- Develop/script.js
+++ Develop/script.js
@@ -1,13 +1,37 @@
 var timeBlock = $(".container");
 var saveBtn = $(".saveBtn");
 var eventInfo = $(".event-info");
+
+setInterval(liveClock, 1000);
+loadText();
+
 function liveClock() {
   const current = moment();
   $("#currentDay").text(current.format("dddd, MMM Do, YYYY, h:mm:ss a"));
 }
-setInterval(liveClock, 1000);
+
+function loadText() {
+  let i;
+  let count;
+  let textBoxValue;
+  for (i = 9; i <= 11; i++) {
+    count = i.toString() + "am";
+    textBoxValue = localStorage.getItem(count);
+    $("#" + count + "Text").val(textBoxValue);
+  }
+
+  textBoxValue = localStorage.getItem("12pm");
+  $("#12pmText").val(textBoxValue);
+
+  for (i = 1; i <= 5; i++) {
+    count = i.toString() + "pm";
+    textBoxValue = localStorage.getItem(count);
+    $("#" + count + "Text").val(textBoxValue);
+  }
+}
 
 saveBtn.on("click", function () {
-  eventInfo.input;
-  localStorage.setItem("event-info", input);
+  const input = $($(this).prev()[0]).val();
+  console.log(input);
+  localStorage.setItem(this.id, input);
 });
